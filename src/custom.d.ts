b53950d3ebf4e58109d598cf1d3d69e472bd1f9d
hayoonPark <the.day0619@gmail.com>
2025-01-24 16:49:27 +0900

    update: png 타입 추가

--- src/custom.d.ts
+++ src/custom.d.ts
@@ -6,4 +6,15 @@ declare module '*.jpg' {
 declare module '*.JPG' {
   const content: string;
   export default content;
+}
+
+// PNG 타입 선언 추가
+declare module '*.png' {
+  const content: string;
+  export default content;
+}
+
+declare module '*.PNG' {
+  const content: string;
+  export default content;
 } 
